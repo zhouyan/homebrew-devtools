c70741d5ec579dc042944f96488458bdfea3a8dc
Yan Zhou <zhouyan@me.com>
2019-11-10 17:43:24 +0800

    remove unused

--- Formula/numpy-mkl.rb
+++ Formula/numpy-mkl.rb
@@ -1,8 +1,9 @@
 class NumpyMkl < Formula
   desc "Package for scientific computing with Python"
   homepage "https://www.numpy.org/"
-  url "https://github.com/numpy/numpy/releases/download/v1.17.1/numpy-1.17.1.tar.gz"
-  sha256 "24d479ebc92f2d1c739622568f0e4d1382c6bf9778505146a370c8e2f5749839"
+  url "https://files.pythonhosted.org/packages/ac/36/325b27ef698684c38b1fe2e546e2e7ef9cecd7037bcdb35c87efec4356af/numpy-1.17.2.zip"
+  sha256 "73615d3edc84dd7c4aeb212fa3748fb83217e00d201875a47327f55363cef2df"
+  head "https://github.com/numpy/numpy.git"
 
   depends_on "python"
 
